351993a8d2c5689019a55398702c1ff0601d2793
MaherBhasvar <maher.daiict@gmail.com>
2019-04-01 20:01:28 +0530

    express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://maher:maher@ec2-13-127-64-177.ap-south-1.compute.amazonaws.com:27017/admin'
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+const users = require('./routes/api/users');
+
+
+
+
+const app = express();
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+//use routes
+app.use('/api/posts',posts);
+app.use('/api/profile',profile);
+app.use('/api/users',users);
+
+
+
+const port = process.env.PORT || 5000; 
+//process.env.PORT for Heroku
+
+app.listen(port, () => console.log(`Server Running on Port ${port}`));
