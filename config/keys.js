04411a4e296db65fb44d4d6043a75db3bf84e356
tarun11Mavani <tarun.11mavani@gmail.com>
2019-04-17 11:59:40 +0530

    tag hyperlink

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/sen',
+    mongoURI: 'mongodb://maher:maher@ec2-13-127-64-177.ap-south-1.compute.amazonaws.com:27017/admin',
     secretOrKey: 'secret'
 };
